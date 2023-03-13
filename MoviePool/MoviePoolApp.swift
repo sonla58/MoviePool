e39b4b32251e219c699cced04a72637966d0ac65
agentshield <anhsonleit@gmail.com>
2023-03-13 17:32:17 +0700

    Initial Commit

---
+++ MoviePool/MoviePoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePoolApp.swift
+//  MoviePool
+//
+//  Created by Anh Son Le on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviePoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
